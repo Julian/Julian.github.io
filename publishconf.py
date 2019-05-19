436d19a606da62b7b581d79c58b78203871b891e
Julian Berman <Julian@GrayVines.com>
2019-05-19 10:53:05 -0400

    Clean template URLs.

--- publishconf.py
+++ publishconf.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 
-# This file is only used if you use `make publish` or
-# explicitly specify it as your config file.
-
 import os
 import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-# If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = "https://julian.github.io"
 RELATIVE_URLS = False
 
