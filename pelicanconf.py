eb71312504145f28d033b7799a66602c6e991ba7
Julian Berman <Julian@GrayVines.com>
2019-05-20 12:07:26 -0400

    Bunch more settingification.

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 AUTHOR = "Julian Berman"
 SITENAME = "Julian"
+TAGLINE ="All the Good Names Were Already Taken, Including This One."
 SITEURL = ""
 
 PATH = "content"
@@ -12,23 +13,25 @@ TIMEZONE = "America/New_York"
 
 DEFAULT_LANG = "en"
 
-# Feed generation is usually not desired when developing
+TYPOGRIFY = True
+
+# Disable feed generation for development only.
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
 LINKS = [
     ("GitHub", "https://github.com/Julian/"),
 ]
 
-# Social widget
 SOCIAL = [
     ("@JulianWasTaken", "https://twitter.com/JulianWasTaken"),
     ("@julianberman", "https://instagram.com/julianberman"),
 ]
+GITHUB_URL = "https://github.com/Julian"
+TWITTER_USERNAME = "JulianWasTaken"
 
 DEFAULT_PAGINATION = 10
 
@@ -36,6 +39,9 @@ THEME = Path(__file__).parent / "martin-pelican"
 
 ARTICLE_URL = "posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/"
 ARTICLE_SAVE_AS = "posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html"
+
+DRAFT_URL = "drafts/{date:%Y}/{date:%m}/{date:%d}/{slug}/"
+DRAFT_SAVE_AS = "drafts/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html"
 
 AUTHOR_URL = 'authors/{slug}/'
 AUTHOR_SAVE_AS = 'authors/{slug}/index.html'
