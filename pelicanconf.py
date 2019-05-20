43edc7bab00efc63d69bec1e6eb81e449835ac09
Julian Berman <Julian@GrayVines.com>
2019-05-20 10:32:57 -0400

    Fix the authors, categories and tag pages.

--- pelicanconf.py
+++ pelicanconf.py
@@ -36,5 +36,18 @@ THEME = Path(__file__).parent / "martin-pelican"
 
 ARTICLE_URL = "posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/"
 ARTICLE_SAVE_AS = "posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html"
+
+AUTHOR_URL = 'authors/{slug}/'
+AUTHOR_SAVE_AS = 'authors/{slug}/index.html'
+AUTHORS_SAVE_AS = 'authors/index.html'
+
+CATEGORY_URL = 'categories/{slug}/'
+CATEGORY_SAVE_AS = 'categories/{slug}/index.html'
+CATEGORIES_SAVE_AS = 'categories/index.html'
+
 PAGE_URL = "{slug}/"
 PAGE_SAVE_AS = "{slug}/index.html"
+
+TAG_URL = 'tags/{slug}/'
+TAG_SAVE_AS = 'tags/{slug}/index.html'
+TAGS_SAVE_AS = 'tags/index.html'
