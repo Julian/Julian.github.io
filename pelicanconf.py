2f113ea72c19a5e0de6fa87625bd5a6183f43084
Julian Berman <Julian@GrayVines.com>
2020-01-25 23:30:15 -0500

    Minor style.

--- pelicanconf.py
+++ pelicanconf.py
@@ -43,17 +43,17 @@ ARTICLE_SAVE_AS = "posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html"
 DRAFT_URL = "drafts/{date:%Y}/{date:%m}/{date:%d}/{slug}/"
 DRAFT_SAVE_AS = "drafts/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html"
 
-AUTHOR_URL = 'authors/{slug}/'
-AUTHOR_SAVE_AS = 'authors/{slug}/index.html'
-AUTHORS_SAVE_AS = 'authors/index.html'
+AUTHOR_URL = "authors/{slug}/"
+AUTHOR_SAVE_AS = "authors/{slug}/index.html"
+AUTHORS_SAVE_AS = "authors/index.html"
 
-CATEGORY_URL = 'categories/{slug}/'
-CATEGORY_SAVE_AS = 'categories/{slug}/index.html'
-CATEGORIES_SAVE_AS = 'categories/index.html'
+CATEGORY_URL = "categories/{slug}/"
+CATEGORY_SAVE_AS = "categories/{slug}/index.html"
+CATEGORIES_SAVE_AS = "categories/index.html"
 
 PAGE_URL = "{slug}/"
 PAGE_SAVE_AS = "{slug}/index.html"
 
-TAG_URL = 'tags/{slug}/'
-TAG_SAVE_AS = 'tags/{slug}/index.html'
-TAGS_SAVE_AS = 'tags/index.html'
+TAG_URL = "tags/{slug}/"
+TAG_SAVE_AS = "tags/{slug}/index.html"
+TAGS_SAVE_AS = "tags/index.html"
